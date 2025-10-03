ba47d55d3be42f75bcd9d7244f221888357747bd
farhandevandrasakti <farhandevandrasakti07@gmail.com>
2025-10-03 11:32:47 +0700

    Modifikasi Percobaan 1

--- jobsheet5/ifCetakKRS12.java
+++ jobsheet5/ifCetakKRS12.java
@@ -10,6 +10,8 @@ public class ifCetakKRS12 {
 
             System.out.println("Pembayaran UKT terverifikasi.");
             System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
+        } else {
+            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
 
         }
     }
