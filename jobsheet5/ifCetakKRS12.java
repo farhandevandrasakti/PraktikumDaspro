c05c2d17f1dfe34a369ececce19c01cae1524cb5
farhandevandrasakti <farhandevandrasakti07@gmail.com>
2025-10-03 21:16:47 +0700

    Tugas 1

--- jobsheet5/ifCetakKRS12.java
+++ jobsheet5/ifCetakKRS12.java
@@ -1,19 +1,24 @@
 package jobsheet5;
 import java.util.Scanner;
+
 public class ifCetakKRS12 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("---Cetak KRS SIAKAD---");
         System.out.print("Apakah UKT sudah lunas? (true/false): ");
         boolean uktLunas = sc.nextBoolean();
+        
         if(uktLunas) {
-
             System.out.println("Pembayaran UKT terverifikasi.");
             System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
-            } else {
+        } else {
             System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
-
-        
         }
+        
+        String hasil = uktLunas ? 
+            "Pembayaran UKT terverifikasi.\nSilakan cetak KRS dan minta tanda tangan DPA" : 
+            "Registrasi ditolak. Silakan lunasi UKT terlebih dahulu";
+        System.out.println("\nHasil dengan Ternary Operator:");
+        System.out.println(hasil);
     }
 }
